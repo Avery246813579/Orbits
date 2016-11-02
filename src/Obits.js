eee885aa6de8fb48f94129cbf1255621115bdc6a
Avery Durrant <Avery246813579@gmail.com>
2016-11-02 11:43:54 -0400

    Physics stuff

--- src/Obits.js
+++ src/Obits.js
@@ -1,7 +1,71 @@
 var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+var planets = [{X: 100, Y: 100, RADIUS: 25}];
 
-canvas.width = canvas.style.width;
-canvas.height = canvas.style.height;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-console.dir(canvas.width);
-console.dir(canvas.height);
+window.onresize = function(event){
+    canvas.width = event.srcElement.innerWidth;
+    canvas.height = event.srcElement.innerHeight;
+};
+
+var globalID;
+var lastTime = new Date().getTime(), tick = new Date().getTime();
+var delta = 0, targetUps = 1000 / 60, frames = 0, updates = 0;
+
+function render() {
+    var now = new Date().getTime();
+    delta += (now - lastTime) / targetUps;
+    lastTime = now;
+
+    if (delta >= 1) {
+        updates++;
+        delta--;
+
+        update();
+    }
+
+    draw();
+    frames++;
+
+    if (now - tick > 1000) {
+        tick = now;
+        frames = 0;
+        updates = 0;
+    }
+
+    globalID = requestAnimationFrame(render);
+}
+
+function draw(){
+    for(var p = 0; p < planets.length; p++){
+        var planet = planets[p];
+
+        console.log("HERE");
+
+        context.beginPath();
+        context.fillStyle = 'green';
+        context.arc(planet.X, planet.Y, planet.RADIUS, 0 , 2 * Math.PI);
+        context.fill();
+        context.stroke();
+    }
+}
+
+function update(){
+
+}
+
+const GRAVITY = 9.8;
+function calculateForce(pRadius, sRadius){
+    return GRAVITY * calculateMass(pRadius) * calculateMass(sRadius) / (sRadius * pRadius);
+}
+
+function calculateMass(radius){
+    return Math.abs(Math.pow(Math.pow(radius, 5), 1/4));
+}
+
+console.dir(calculateForce(5, 25 ));
+console.dir(calculateMass(25));
+
+requestAnimationFrame(render);
